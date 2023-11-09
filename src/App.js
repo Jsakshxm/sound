fb511e014081662e7d8af21415890b7d6c215489
Saksham jain <130843255+Jsakshxm@users.noreply.github.com>
2023-11-09 17:08:22 +0530

    hii

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import audioFile from '/Users/saksham/Downloads/untitledfolder/my-app/src/AwakendEmsizzleREVISED.mp3'; // Adjust the path to match your project structure
 
 const App = () => {
   const audioRef = useRef(null);
 
-  const playAudio = () => {
+  useEffect(() => {
     const audio = audioRef.current;
-    if (audio) {
-      audio.play()
-        .then(() => {
-          console.log('Audio is playing');
-        })
-        .catch(error => {
-          console.error('Audio playback failed: ', error);
-        });
-    }
-  };
+
+    const playAudio = () => {
+      if (audio) {
+        audio.play()
+          .then(() => {
+            console.log('Audio is playing');
+          })
+          .catch(error => {
+            console.error('Audio playback failed: ', error);
+          });
+      }
+    };
+
+    // Start playing the audio once the component is mounted
+    document.addEventListener('click', playAudio, { once: true });
+
+    return () => {
+      document.removeEventListener('click', playAudio);
+    };
+  }, []);
 
   return (
     <div>
       <h1>My Website</h1>
-      <button onClick={playAudio}>Play Audio</button>
       <audio ref={audioRef} src={audioFile} />
     </div>
   );
